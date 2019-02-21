04a5006b95eeeb0f1b9c17e3734499877a538d1e
Sergio de Carvalho <scarvalhojr@gmail.com>
2019-02-21 21:56:31 +0000

    Rust - Macros

---
+++ rust/macros/src/lib.rs
@@ -0,0 +1,13 @@
+#[macro_export]
+macro_rules! hashmap {
+    ($($key:expr => $value:expr,)+) => { hashmap!($($key => $value),+) };
+    ($($key:expr => $value:expr),*) => {
+        {
+            let mut temp_map = ::std::collections::HashMap::new();
+            $(
+                temp_map.insert($key, $value);
+            )*
+            temp_map
+        }
+    };
+}
