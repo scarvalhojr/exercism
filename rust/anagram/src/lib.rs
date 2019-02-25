0b9c4c668b7b32c596727370fd34e36c5be824e7
Sergio de Carvalho <scarvalhojr@gmail.com>
2019-02-25 23:41:55 +0000

    Rust - Anagram simplified

--- rust/anagram/src/lib.rs
+++ rust/anagram/src/lib.rs
@@ -1,44 +1,24 @@
-use std::collections::{HashMap, HashSet};
-use std::str::FromStr;
+use std::collections::HashSet;
 
 pub fn anagrams_for<'a>(
     word: &str,
-    possible_anagrams: &[&'a str],
+    candidates: &[&'a str],
 ) -> HashSet<&'a str> {
-    let target: Word = word.parse().unwrap();
-    possible_anagrams
+    let target = word.to_lowercase();
+    let sorted_chars = sort_chars(&target);
+
+    candidates
         .iter()
-        .filter(|s| target.is_anagram(&s.parse().unwrap()))
         .cloned()
+        .filter(|s| {
+            let candidate = s.to_lowercase();
+            candidate != target && sort_chars(&candidate) == sorted_chars
+        })
         .collect()
 }
 
-struct Word {
-    lowercase: String,
-    char_count: HashMap<char, usize>,
-}
-
-impl Word {
-    fn is_anagram(&self, other: &Word) -> bool {
-        self.lowercase != other.lowercase && self.char_count == other.char_count
-    }
-}
-
-impl FromStr for Word {
-    type Err = String;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let lowercase = s.to_lowercase();
-        let mut char_count = HashMap::new();
-        for ch in lowercase.chars() {
-            char_count
-                .entry(ch)
-                .and_modify(|count| *count += 1)
-                .or_insert(1);
-        }
-        Ok(Self {
-            lowercase,
-            char_count,
-        })
-    }
+fn sort_chars(word: &str) -> Vec<char> {
+    let mut chars: Vec<_> = word.chars().collect();
+    chars.sort_unstable();
+    chars
 }
