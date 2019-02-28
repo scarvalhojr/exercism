9583b6eb31b8f59457dd4042f08aee68e7639ef4
Sergio de Carvalho <scarvalhojr@gmail.com>
2019-02-28 23:39:12 +0000

    Rust - Parallel Letter Frequency experiment
    Using Crossbeam scope and channel. This is slower than using Rayon's
    parallel iterators, although it is more correct as it guarantees the
    exact number of threads requested.

--- rust/parallel-letter-frequency/src/lib.rs
+++ rust/parallel-letter-frequency/src/lib.rs
@@ -1,36 +1,39 @@
-use rayon::iter::ParallelIterator;
-use rayon::slice::ParallelSlice;
+use crossbeam::scope;
+use crossbeam::channel::unbounded;
 use std::collections::HashMap;
 
 pub fn frequency(input: &[&str], worker_count: usize) -> HashMap<char, usize> {
+    let mut totals = HashMap::new();
     if input.is_empty() || worker_count == 0 {
-        return HashMap::new();
+        return totals;
     }
 
-    // The input is split into chunks, one for each thread (worker); while this
-    // ensures the number of threads will be less than worker_count, there is
-    // no guarantee that the requested number of threads will actually be spawn
-    // as we're relying on Rayon's global threadpool; the only way to guarantee
-    // the exact number of threads would be to setup a new thread pool on each
-    // call but this is expensive and would make this function much slower
+    let (tx, rx) = unbounded();
     let chunk_size = (input.len() as f32 / worker_count as f32).ceil() as usize;
-    input
-        .par_chunks(chunk_size)
-        .map(|chunk| {
-            let mut counters = HashMap::new();
-            for line in chunk {
-                for ch in
-                    line.to_lowercase().chars().filter(|c| c.is_alphabetic())
-                {
-                    *counters.entry(ch).or_insert(0) += 1;
+    scope(|scope| {
+        for chunk in input.chunks(chunk_size) {
+            let tx = tx.clone();
+            scope.spawn(move |_| {
+                let mut counters = HashMap::new();
+                for line in chunk {
+                    line.to_lowercase()
+                        .chars()
+                        .filter(|chr| chr.is_alphabetic())
+                        .for_each(|chr| {
+                            *counters.entry(chr).or_insert(0) += 1;
+                        })
                 }
+                tx.send(counters).unwrap();
+            });
+        }
+
+        drop(tx);
+        for counters in rx {
+            for (chr, count) in counters {
+                *totals.entry(chr).or_insert(0) += count;
             }
-            counters
-        })
-        .reduce(HashMap::new, |mut counters_a, counters_b| {
-            for (ch, count) in counters_b {
-                *counters_a.entry(ch).or_insert(0) += count;
-            }
-            counters_a
-        })
+        }
+    }).unwrap();
+
+    totals
 }
