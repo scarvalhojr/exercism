9381e7d904facafce01623b879b43ce4cd48ed67
Sergio de Carvalho <scarvalhojr@gmail.com>
2019-02-28 20:39:50 +0000

    Rust - Parallel Letter Frequency v2
    Using Rayon's map & reduce parallel iterators

--- rust/parallel-letter-frequency/src/lib.rs
+++ rust/parallel-letter-frequency/src/lib.rs
@@ -1,35 +1,36 @@
 use rayon::iter::{IntoParallelIterator, ParallelIterator};
 use rayon::ThreadPoolBuilder;
 use std::collections::HashMap;
-use std::sync::mpsc::channel;
 
 pub fn frequency(input: &[&str], worker_count: usize) -> HashMap<char, usize> {
-    let (tx, rx) = channel();
-    let pool = ThreadPoolBuilder::new()
+    ThreadPoolBuilder::new()
         .num_threads(worker_count)
         .build()
-        .unwrap();
-    pool.install(|| {
-        input.into_par_iter().for_each_with(tx, |sender, line| {
-            let mut counters = HashMap::new();
-            for ch in line.to_lowercase().chars().filter(|c| c.is_alphabetic())
-            {
-                counters.entry(ch).and_modify(|n| *n += 1).or_insert(1);
-            }
-            sender.send(counters).unwrap();
-        });
-    });
-
-    let mut totals = HashMap::new();
-    for mut counters in rx {
-        for (ch, count) in counters.drain() {
-            totals
-                .entry(ch)
-                .and_modify(|n| *n += count)
-                .or_insert(count);
-        }
-    }
-    totals
+        .unwrap()
+        .install(|| {
+            input
+                .into_par_iter()
+                .map(|line| {
+                    let mut counters = HashMap::new();
+                    for ch in line
+                        .to_lowercase()
+                        .chars()
+                        .filter(|c| c.is_alphabetic())
+                    {
+                        counters.entry(ch).and_modify(|n| *n += 1).or_insert(1);
+                    }
+                    counters
+                })
+                .reduce(HashMap::new, |mut counters_a, mut counters_b| {
+                    for (ch, count) in counters_b.drain() {
+                        counters_a
+                            .entry(ch)
+                            .and_modify(|n| *n += count)
+                            .or_insert(count);
+                    }
+                    counters_a
+                })
+        })
 }
 
 pub fn frequency_seq(
