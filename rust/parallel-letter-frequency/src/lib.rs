398f91ad90f856aef40c7796f519914730f055c3
Sergio de Carvalho <scarvalhojr@gmail.com>
2019-02-28 22:08:49 +0000

    Rust - Parallel Letter Frequency v4
    Minor improvements

--- rust/parallel-letter-frequency/src/lib.rs
+++ rust/parallel-letter-frequency/src/lib.rs
@@ -7,6 +7,12 @@ pub fn frequency(input: &[&str], worker_count: usize) -> HashMap<char, usize> {
         return HashMap::new();
     }
 
+    // The input is split into chunks, one for each thread (worker); while this
+    // ensures the number of threads will be less than worker_count, there is
+    // no guarantee that the requested number of threads will actually be spawn
+    // as we're relying on Rayon's global threadpool; the only way to guarantee
+    // the exact number of threads would be to setup a new thread pool on each
+    // call but this is expensive and would make this function much slower
     let chunk_size = (input.len() as f32 / worker_count as f32).ceil() as usize;
     input
         .par_chunks(chunk_size)
@@ -16,17 +22,14 @@ pub fn frequency(input: &[&str], worker_count: usize) -> HashMap<char, usize> {
                 for ch in
                     line.to_lowercase().chars().filter(|c| c.is_alphabetic())
                 {
-                    (*counters.entry(ch).or_insert(0)) += 1;
+                    *counters.entry(ch).or_insert(0) += 1;
                 }
             }
             counters
         })
-        .reduce(HashMap::new, |mut counters_a, mut counters_b| {
-            for (ch, count) in counters_b.drain() {
-                counters_a
-                    .entry(ch)
-                    .and_modify(|n| *n += count)
-                    .or_insert(count);
+        .reduce(HashMap::new, |mut counters_a, counters_b| {
+            for (ch, count) in counters_b {
+                *counters_a.entry(ch).or_insert(0) += count;
             }
             counters_a
         })
