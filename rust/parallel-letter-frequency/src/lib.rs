fc8870eca55491b4878b24a5605d79529cab6f1b
Sergio de Carvalho <scarvalhojr@gmail.com>
2019-02-28 21:24:27 +0000

    Rust - Parallel Letter Frequency v3
    * Use Rayon's global thread pool to avoid having to setup a new pool on
    each call
    * Split the input slice into chunks based on the worker_count argument;
      while this puts an upper bound on the number of threads, it does not
      guarantee that the requested number of threads will actually be used

--- rust/parallel-letter-frequency/src/lib.rs
+++ rust/parallel-letter-frequency/src/lib.rs
@@ -1,47 +1,33 @@
-use rayon::iter::{IntoParallelIterator, ParallelIterator};
-use rayon::ThreadPoolBuilder;
+use rayon::iter::ParallelIterator;
+use rayon::slice::ParallelSlice;
 use std::collections::HashMap;
 
 pub fn frequency(input: &[&str], worker_count: usize) -> HashMap<char, usize> {
-    ThreadPoolBuilder::new()
-        .num_threads(worker_count)
-        .build()
-        .unwrap()
-        .install(|| {
-            input
-                .into_par_iter()
-                .map(|line| {
-                    let mut counters = HashMap::new();
-                    for ch in line
-                        .to_lowercase()
-                        .chars()
-                        .filter(|c| c.is_alphabetic())
-                    {
-                        counters.entry(ch).and_modify(|n| *n += 1).or_insert(1);
-                    }
-                    counters
-                })
-                .reduce(HashMap::new, |mut counters_a, mut counters_b| {
-                    for (ch, count) in counters_b.drain() {
-                        counters_a
-                            .entry(ch)
-                            .and_modify(|n| *n += count)
-                            .or_insert(count);
-                    }
-                    counters_a
-                })
-        })
-}
-
-pub fn frequency_seq(
-    input: &[&str],
-    _worker_count: usize,
-) -> HashMap<char, usize> {
-    let mut counters = HashMap::new();
-    for line in input {
-        for ch in line.to_lowercase().chars().filter(|c| c.is_alphabetic()) {
-            counters.entry(ch).and_modify(|n| *n += 1).or_insert(1);
-        }
+    if input.is_empty() || worker_count == 0 {
+        return HashMap::new();
     }
-    counters
+
+    let chunk_size = (input.len() as f32 / worker_count as f32).ceil() as usize;
+    input
+        .par_chunks(chunk_size)
+        .map(|chunk| {
+            let mut counters = HashMap::new();
+            for line in chunk {
+                for ch in
+                    line.to_lowercase().chars().filter(|c| c.is_alphabetic())
+                {
+                    (*counters.entry(ch).or_insert(0)) += 1;
+                }
+            }
+            counters
+        })
+        .reduce(HashMap::new, |mut counters_a, mut counters_b| {
+            for (ch, count) in counters_b.drain() {
+                counters_a
+                    .entry(ch)
+                    .and_modify(|n| *n += count)
+                    .or_insert(count);
+            }
+            counters_a
+        })
 }
